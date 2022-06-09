95e203f97fa3a6ed56c678a2424fc412e8e6fbe1
Joshua Hellard <joshua.hellard0@gmail.com>
2022-06-09 14:56:12 -0400

    Work on hamburger menu, fixing versions.
    Moving dev machines, need to save some random changes.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPeace, faBars } from '@fortawesome/free-solid-svg-icons'
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className='container'>
-      <p className='port-ver'>portfolio v0.1.0 // Joshua Hellard</p>
+    <div className="container">
+      <header>
+        <FontAwesomeIcon className='hamburger' icon={faBars} />
+      </header>
+      <div className="landing">
+        <span className="landing__text">
+          <h1>Hello {<FontAwesomeIcon icon={faHandPeace} />}</h1>
+          
+          <h3>I'm Joshua Hellard // Full Stack Developer</h3>
+        </span>
+        <span className="landing__image">
+          <img
+            src="https://pbs.twimg.com/profile_images/1455256619768446977/BrJfRIoG_400x400.jpg"
+            alt="Landing"
+          />
+        </span>
+      </div>
+      <footer className="footer">
+        <p className="port-ver">portfolio v0.1.0 // Joshua Hellard</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
